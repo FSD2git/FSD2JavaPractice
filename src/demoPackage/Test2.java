5210385848f65b7818305d8dfc89cc3273aa341e
fsd2git <fsd2gitpractice@gmail.com>
2023-07-12 08:10:03 +0530

    Modified testclass2 for new branch

--- src/demoPackage/Test2.java
+++ src/demoPackage/Test2.java
@@ -3,7 +3,8 @@ package demoPackage;
 public class Test2 {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello World !!");
+		
+		System.out.println("code for new branch");
 	}
 
 }
